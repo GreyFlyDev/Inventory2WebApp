90c3a923eca560a41832d537d96f89c9c3b1a03f
GreyFlyDev <gregnnylf94@gmail.com>
2016-08-04 20:31:06 -0400

    Added All Models

---
+++ InventoryWebApp/InventoryWebApp/Models/Restock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWebApp.Models
+{
+    public class Restock
+    {
+        public int RestockId { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal TotalCost { get; set; }
+        public DateTime Date { get; set; }
+
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ InventoryWebApp/InventoryWebApp/Models/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWebApp.Models
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal SalePricePerUnit { get; set; }
+        public decimal TotalCost { get; set; }
+
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+    }
+}
